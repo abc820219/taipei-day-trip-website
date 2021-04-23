d9db16965ab9bdd9af77e6fa4b9d630fc616b064
abc820219 <z27089433@gmail.com>
2021-04-23 23:00:29 +0800

    python後端 api完成

--- app.py
+++ app.py
@@ -1,20 +1,99 @@
 from flask import *
-app=Flask(__name__)
-app.config["JSON_AS_ASCII"]=False
-app.config["TEMPLATES_AUTO_RELOAD"]=True
+import math
+import mysql.connector
+# 連線
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="website"
+)
+mycursor = mydb.cursor()
+
+app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Pages
+
+
 @app.route("/")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
+
+
 @app.route("/attraction/<id>")
 def attraction(id):
-	return render_template("attraction.html")
+    return render_template("attraction.html")
+
+
 @app.route("/booking")
 def booking():
-	return render_template("booking.html")
+    return render_template("booking.html")
+
+
 @app.route("/thankyou")
 def thankyou():
-	return render_template("thankyou.html")
+    return render_template("thankyou.html")
+
+# API
+@app.route("/api/attractions")
+def getAttractionsHandler():
+    keyword = request.args.get('keyword')
+    page = int(request.args.get('page'))
+    if keyword:
+        sql = f"select count(*) from attractions where name like '%{keyword}%'"
+    else:
+        sql = f"select count(*) from attractions"
+
+    mycursor.execute(sql)
+    perpage = 12
+    totalPage = mycursor.fetchone()[0]
+    lastPage = math.floor(totalPage / perpage)
+    startPage = perpage * page
+
+    if page > lastPage:
+        startPage = 0
+
+    if keyword:
+        sql = f"select * from attractions where name like '%{keyword}%' limit {startPage}, {perpage}"
+    else:
+        sql = f"select * from attractions limit {startPage}, {perpage}"
+
+    mycursor.execute(sql)
+    data = mycursor.fetchall()
+    nextPage = page + 1
+
+    if nextPage > lastPage:
+        nextPage = 0
+
+    result = {}
+    result['data'] = []
+    result['page'] = nextPage
+
+    for i in range(len(data)):
+        rowData = {}
+        rowData['pid'] = data[i][0]
+        rowData['id'] = data[i][1]
+        rowData['name'] = data[i][2]
+        rowData['category'] = data[i][3]
+        rowData['description'] = data[i][4]
+        rowData['address'] = data[i][5]
+        rowData['transport'] = data[i][6]
+        rowData['mrt'] = data[i][7]
+        rowData['latitude'] = data[i][8]
+        rowData['longitude'] = data[i][9]
+        rowData['images'] = data[i][10]
+        result['data'].insert(i, rowData)
+
+    return result
+
+
+@app.route("/api/attraction/<attractionId>")
+def attractionHandler(attractionId):
+    sql = f"select * from attractions where id like '{attractionId}'"
+    mycursor.execute(sql)
+    result = mycursor.fetchone()
+    return json.dumps(result)
 
 app.run(port=3000)
---
+++ data/exportToMysql.py
@@ -0,0 +1,77 @@
+from flask import *
+import mysql.connector
+import json
+import re
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="website"
+)
+mycursor = db.cursor()
+
+# def dropTable(name):
+#     sql = f"DROP TABLE {name}"
+#     mycursor.execute(sql)
+# dropTable("attractions")
+
+
+def createAttractionsTable():
+    sql = "create table attractions(pid bigint not null auto_increment primary key comment '流水號',id int not null, name varchar(255) not null comment '景點名稱', category varchar(255) comment '景點分類', description text comment '景點描述', address varchar(255) comment '景點地址', transport text comment '交通方式', mrt varchar(255) comment '捷運', latitude double comment '緯度', longitude double comment '經度', images text comment '圖片')"
+    mycursor.execute(sql)
+createAttractionsTable()
+
+
+def insertAttractionsSql(i):
+    sql = None
+    if i != None:
+        for i in range(len(file)):
+            if i > 84:
+                id = file[i]["_id"]
+                name = file[i]["stitle"]
+                category = file[i]["CAT2"]
+                description = file[i]["xbody"]
+                address = file[i]["address"]
+                transport = file[i]["info"]
+                mrt = file[i]["MRT"]
+                latitude = file[i]["latitude"]
+                longitude = file[i]["longitude"]
+                imagesData = file[i]["file"].split('http')
+                images = []
+                for image in imagesData:
+                    if(re.search('jpg', image, re.I) != None):
+                        src = f"http{image}"
+                        images = images + [src]
+                images = json.dumps(images)
+                sql = f"INSERT INTO attractions VALUES (null,'{id}','{name}','{category}','{description}','{address}','{transport}','{mrt}','{latitude}','{longitude}','{images}')"
+                mycursor.execute(sql)
+                db.commit()
+    else:
+        for line in file:
+            id = line["_id"]
+            name = line["stitle"]
+            category = line["CAT2"]
+            description = line["xbody"]
+            address = line["address"]
+            transport = line["info"]
+            mrt = line["MRT"]
+            latitude = line["latitude"]
+            longitude = line["longitude"]
+            imagesData = line["file"].split('http')
+            images = []
+            for image in imagesData:
+                if(re.search('jpg', image, re.I) != None):
+                    src = f"http{image}"
+                    images = images + [src]
+            images = json.dumps(images)
+            # print(images)
+            sql = f"INSERT INTO attractions VALUES (null,'{id}','{name}','{category}','{description}','{address}','{transport}','{mrt}','{latitude}','{longitude}','{images}')"
+            mycursor.execute(sql)
+            db.commit()
+
+
+with open("taipei-attractions.json", mode="r", encoding="utf-8") as file:
+    file = json.load(file)
+    file = file["result"]["results"]
+    insertAttractionsSql(None)
