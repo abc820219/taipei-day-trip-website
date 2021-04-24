bd1c7edaee397d8ed02eaa44631d3278f84b971e
abc820219 <z27089433@gmail.com>
2021-04-24 20:16:03 +0800

    app password change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="root",
+    password="z27089433",
     database="website"
 )
 mycursor = mydb.cursor()
