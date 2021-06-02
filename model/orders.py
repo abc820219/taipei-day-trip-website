e400f96d7c99028d78d913817ba8ca9465d15857
abc820219 <z27089433@gmail.com>
2021-06-02 23:21:11 +0800

    前端優化

--- model/orders.py
+++ model/orders.py
@@ -1,15 +1,21 @@
 import mysql.connector
+from mysql.connector import pooling
 import os
 from dotenv import load_dotenv
 from flask import *
 load_dotenv()
 # 連線
-mydb = mysql.connector.connect(
+poolname = "mysqlpool"
+poolsize = 3
+connectionpool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name=poolname,
+    pool_reset_session=True,
     host=os.getenv("SERVER_DB_HOST"),
     user=os.getenv("SERVER_DB_USER"),
     password=os.getenv("SERVER_DB_PASSWORD"),
     database=os.getenv("SERVER_DB_DATABASE")
 )
+mydb = connectionpool.get_connection()
 mycursor = mydb.cursor()
 
 # sqlFunction
